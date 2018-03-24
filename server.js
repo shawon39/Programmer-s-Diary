6cdaf16764330eb207bc53fbe1b63fa5c591f1f8
shawon <shshawon39@gmail.com>
2018-03-24 22:32:17 +0600

    Second commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = (`${now}: ${req.method} ${req.url}`);
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+app.use(express.static(__dirname + '/'));
+
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
