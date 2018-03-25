0615cca63a52c1b5e9e6acb7d6fb46d416b531b4
shawon <shshawon39@gmail.com>
2018-03-26 01:00:21 +0600

    Five commit

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const fs = require('fs');
+var pg = require('pg');
+var app = express();
+
+// Database Connection !
+var conString = "postgres://postgres:sh56348635@localhost:5432/PDiary";
+var client = new pg.Client(conString);
+client.connect();
+
+client.query('SELECT * from test', (err, res) => {
+  if (err) {
+    console.log(err.stack);
+  } else {
+    console.log(res.rows[0].name);
+  }
+});
 
 const port = process.env.PORT || 3000;
 var app = express();
